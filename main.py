74116adb16865161f16670ed7d480d009f53d2b4
fiza__02 <mdyasmin0210@gmail.com>
2023-10-12 04:20:14 +0530

    class-based decorators

--- main.py
+++ main.py
@@ -1,46 +1,13 @@
-"""
-print("Hello CodeSandbox!")
+class decorated_class(object):
+    #self for instance
+    def __init__(self,original_function):
+        self.original_function=original_function
+    def __call__(self,*args,**kwargs):
+        print('call method executed this before {}'.format(self.original_function.__name__))
+        return self.original_function(*args,**kwargs)
 
-def outer_function():
-    message= "hi" #free variable
-
-    def inner_function():
-        print(message)
-    return inner_function
-my_func=outer_function()
-my_func()
-my_func()
-
-"""
-
-
-def decorator_function(original_function):
-    def wrapper_function(*args,**kwargs):
-        print('wrapper executed this before {}'.format(original_function.__name__))
-        return original_function(*args,**kwargs)
-    return wrapper_function
-
-@decorator_function #display= decorator_function(display)
+@decorated_class
 def display():
     print('display function ran')
-# display= decorator_function(display)
+
 display()
-#this is a decorator.
-
-@decorator_function
-def display_info(name,age):
-    print('display_info ran with arguments ({},{})'.format(name,age))
-
-'''
-    display_info("viswa",23)
-TypeError: decorator_function.<locals>.wrapper_function() takes 0 positional arguments but 2 were given
-display_info("viswa",23)
-'''
-display_info("viswa",23)
-'''
-output:
-wrapper executed this before display_info
-display_info ran with arguments (viswa,23)
-'''
-# my_s=decorator_function("kiko")
-# my_s()
